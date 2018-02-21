11c7bae71a9400c3426ed4d3b508ea962888e69e
Dawid0x0 <34715900+Dawid0x0@users.noreply.github.com>
2018-02-21 16:56:02 +0100

    Delete signals.py

--- brewery/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from django.utils.text import slugify
-from .models import Place
-
-@receiver(pre_save, sender=Place)
-def my_handler(sender, instance, *args, **kwargs):
-    if not instance.slug:
-		instance.slug = slugify(instance.name)
