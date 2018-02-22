69502a29e840ad92e8a4964df77621a2b76b1d97
Dawid <dawid.m.musial@gmail.com>
2018-02-22 17:48:27 +0000

    Set admin view

--- brewery/admin.py
+++ brewery/admin.py
@@ -4,8 +4,24 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Place
 
+@admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    pass
-    #prepopulated_fields = {'slug': ('name',)}
-
-admin.site.register(Place,PlaceAdmin)
+    ordering = ('-timestamps','name')
+    empty_value_display = 'unknown'
+    fieldsets = (
+        (None,{
+            'fields':('name','description')
+        }),
+        ('Adress',{
+            'fields':('city','street','number','www')
+        }),
+        ('Adwanced',{
+            'classes': ('collapse',),
+            'fields':('slug',)
+        }),
+    )
+    
+    list_display = ('name','city','www')
+    list_filter = ('city','timestamps')
+    readonly_fields = ('slug',)
+    search_fields = ('name',)
